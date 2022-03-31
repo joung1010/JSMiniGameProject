c348a20a4a184100031aef97b7e75071cd71f9ae
joung1010 <79488695+joung1010@users.noreply.github.com>
2022-03-31 20:21:36 +0900

    1.시작버튼 기능 추가

--- clone/js/main.js
+++ clone/js/main.js
@@ -1,15 +1,70 @@
 'use strict';
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
 
-function initGame() {
-    //벌레와 당근을 생성한뒤 field에 추가하자
-    console.log(fieldRect);
-    addItem('carrot',5,'img/carrot.png')
-    addItem('bug',5,'img/bug.png')
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click',event=>{
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopBtn();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+
+function stopGame() {
 
 }
+
+function showStopBtn() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+// innerText vs textContent
+//innerText는 'Element'의 속성으로, 해당 Element 내에서 사용자에게 '보여지는' 텍스트 값을 읽어옵니다
+//textContent는 'Node'의 속성으로, innetText와는 달리 <script>나 <style> 태그와 상관없이
+// 해상 노드가 가지고 있는 텍스트 값을 그대로 읽습니다.
+//또한, 'display:none' 스타일이 적용된 '숨겨진 텍스트' 문자열도 그대로 출력되는 것을 확인 할 수 있습니다.
+//
+
+
+function initGame() {
+    field.innerHTML='';
+    gameScore.innerText =CARROT_COUNT;
+    //벌레와 당근을 생성한뒤 field에 추가하자
+    addItem('carrot',CARROT_COUNT,'img/carrot.png')
+    addItem('bug',BUG_COUNT,'img/bug.png')
+}
+
+function startGameTimer() {
+
+}
+
 
 function addItem(className, count, imgPath) {
     const x1 = 0;
@@ -34,4 +89,3 @@ function randomNumber(min,max){
     return Math.random() * (max - min) + min;
 }
 
-initGame();
